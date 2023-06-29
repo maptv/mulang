c60555ae412f1c3da88d9fd4b4ac44f51fdd40e4
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2023-06-29 17:40:10 -0400

    add shell code that generates the html previews of the jupyter notebooks

--- update.sh
+++ update.sh
@@ -7,3 +7,7 @@ quarto render "$1" --profile python --metadata engine:jupyter --output "${1%.*}_
 quarto render "$1" --profile r --metadata engine:jupyter --output "${1%.*}_r.ipynb"
 
 quarto render "$1" --profile knitr --metadata engine:knitr
+
+printf "Python code:\n\n{{< embed ${1%.*}_python.ipynb echo=true >}}\n\n# R code:\n\n{{< embed ${1%.*}_r.ipynb echo=true >}}" > "${1%.*}_embed.qmd"
+
+quarto render "${1%.*}_embed.qmd" --profile embed --metadata engine:knitr --to html
