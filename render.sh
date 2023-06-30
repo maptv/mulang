89a02c321daa88bdac6ff3f22c353fc46ec2df9a
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2023-06-30 12:16:20 -0400

    replacing code chunks with embed shortcodes is working for julia, python, and R

--- render.sh
+++ render.sh
@@ -6,18 +6,34 @@ quarto render "$1" --profile python --metadata engine:jupyter --output "${1%.*}_
 
 quarto render "$1" --profile r --metadata engine:jupyter --output "${1%.*}_quarto_r.ipynb"
 
+quarto render "$1" --profile julia --metadata engine:jupyter --output "${1%.*}_quarto_julia.ipynb"
+
 jupytext "${1%.*}_quarto_python.ipynb" --to py --output "${1%.*}.py" --set-kernel python3
 
 jupytext "${1%.*}_quarto_r.ipynb" --to R --output "${1%.*}.R" --set-kernel ir
+
+jupytext "${1%.*}_quarto_julia.ipynb" --to jl --output "${1%.*}.jl" --set-kernel julia-1.9
 
 jupytext "${1%.*}.py" --output "${1%.*}_jupytext_python.ipynb" --execute --set-kernel python3
 
 jupytext "${1%.*}.R" --output "${1%.*}_jupytext_r.ipynb" --execute --set-kernel ir
 
-# WIP: replace code chunks with embed short codes
-# cat mulang.qmd | sed 's/```{\(.*\)}/{{< embed mulang_\1/g;s/#| tags: \[\(.*\)\]/#\1.ipynb >}}/g'\n
+jupytext "${1%.*}.jl" --output "${1%.*}_jupytext_julia.ipynb" --execute --set-kernel julia-1.9
 
-# cat mulang.qmd | awk -v RS='```' '!/{julia}|{python}|{r}/' > "${1%.*}_embed.qmd"
+# replace code chunks with embed short codes
+cat "${1%.*}.qmd"  | gsed '/^```{/,/^```$/ { # set a range and say what should happen in that range below
+    /^```{.*}$\|#| tags:/!d; # delete all but the first line and tags line of all code chunks
+    N;s/\n//g; # join pairs of lines in all code blocks
+    s/^```{\(.*\)}#| tags: \[\(.*\)\]/{{< embed _\1\.ipynb#\2 >}}/g; # replace code chunk and comment syntax with shortcode syntax
+    /{{< embed .*\.ipynb#.* >}}/!d # delete anything that is not an embed shortcode with a tag
+}' | sed "s/{{< embed \(.*\)\.ipynb#\(.*\) >}}/{{< embed ${1%.*}_jupytext\1\.ipynb#\2 >}}/g"> "${1%.*}_embed.qmd"
+
+# Here is the above command in a single line.
+# replace code chunks with embed short codes by setting a range, deleting all but two lines, joining the lines, substituting, and deleting anything that does not match the shortcode pattern
+# cat "${1%.*}.qmd" | gsed '/^```{/,/^```$/ { /^```{.*}$\|#| tags:/!d; N;s/\n//g; s/^```{\(.*\)}#| tags: \[\(.*\)\]/{{< embed _\1\.ipynb#\2 >}}/g; /{{< embed .*\.ipynb#.* >}}/!d }' > "${1%.*}_embed.qmd"
+
+# Ranges example: This deletes all code chunks
+# cat mulang.qmd | sed '/^```{/,/^```$/d'\n
 
 # printf "\n\n# Python code:\n\n{{< embed ${1%.*}_python.ipynb echo=true >}}\n\n# R code:\n\n{{< embed ${1%.*}_r.ipynb echo=true >}}" >> "${1%.*}_embed.qmd"
 
@@ -25,4 +41,4 @@ jupytext "${1%.*}.R" --output "${1%.*}_jupytext_r.ipynb" --execute --set-kernel 
 quarto render "${1%.*}.qmd" --profile knitr --metadata engine:knitr
 
 # recreate html output file with embedded notebooks
-# quarto render "${1%.*}_embed.qmd" --profile knitr --metadata engine:knitr --to html --output "${1%.*}.html"
+quarto render "${1%.*}_embed.qmd" --profile knitr --metadata engine:knitr --to html --output "${1%.*}.html"
